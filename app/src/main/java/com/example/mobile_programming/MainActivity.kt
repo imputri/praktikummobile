21d59a80340d6ad11a5fd18a7c38c178dff38a0f
imputri <martalitalitaputrizega.gmail.com>
2023-09-18 10:26:13 +0530

    kalkulator sederhana

--- app/src/main/java/com/example/mobile_programming/MainActivity.kt
+++ app/src/main/java/com/example/mobile_programming/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.mobile_programming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var panjang: EditText
+    private lateinit var lebar: EditText
+    private lateinit var tinggi: EditText
+    private lateinit var btn: Button
+    private lateinit var teks_hasil: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        panjang= findViewById(R.id.edt_length)
+        lebar= findViewById(R.id.edt_width)
+        tinggi= findViewById(R.id.edt_height)
+        btn= findViewById(R.id.btn_result)
+        teks_hasil= findViewById(R.id.result)
+
+        btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id==R.id.result){
+            val panjang = panjang.text.toString().trim()
+            val lebar = lebar.text.toString().trim()
+            val tinggi = tinggi.text.toString().trim()
+            val volume = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
+            teks_hasil.text = volume.toString()
+        }
     }
 }
