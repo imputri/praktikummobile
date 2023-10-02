1d21933d24508add1eab091aede52e9191b60a98
imputri <martalitalitaputrizega.gmail.com>
2023-10-02 08:42:51 +0530

    selesai

--- app/src/main/java/com/example/mobile_programming/MainActivity.kt
+++ app/src/main/java/com/example/mobile_programming/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobile_programming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,26 +16,46 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btn: Button
     private lateinit var teks_hasil: TextView
 
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        panjang= findViewById(R.id.edt_length)
-        lebar= findViewById(R.id.edt_width)
-        tinggi= findViewById(R.id.edt_height)
-        btn= findViewById(R.id.btn_result)
-        teks_hasil= findViewById(R.id.result)
-
+        panjang = findViewById(R.id.edt_length)
+        lebar = findViewById(R.id.edt_width)
+        tinggi = findViewById(R.id.edt_height)
+        btn = findViewById(R.id.btn_result)
+        teks_hasil = findViewById(R.id.result)
         btn.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            teks_hasil.text = savedInstanceState.getString(STATE_RESULT)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, teks_hasil.text.toString())
     }
 
     override fun onClick(v: View?) {
-        if (v?.id==R.id.result){
-            val panjang = panjang.text.toString().trim()
-            val lebar = lebar.text.toString().trim()
-            val tinggi = tinggi.text.toString().trim()
-            val volume = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
-            teks_hasil.text = volume.toString()
+        if (v?.id == R.id.btn_result) {
+            val panjangText = panjang.text.toString().trim()
+            val lebarText = lebar.text.toString().trim()
+            val tinggiText = tinggi.text.toString().trim()
+
+            if (panjangText.isNotEmpty() && lebarText.isNotEmpty() && tinggiText.isNotEmpty()) {
+                val panjangValue = panjangText.toDouble()
+                val lebarValue = lebarText.toDouble()
+                val tinggiValue = tinggiText.toDouble()
+                val volume = panjangValue * lebarValue * tinggiValue
+                teks_hasil.text = volume.toString()
+            } else {
+                teks_hasil.text = "Mohon isi semua input"
+            }
         }
     }
 }
